c3bb182639cbef22eff13bd64180c7a5d8c00db4
Shivani-Bahuguna <89532731+Shivani-Bahuguna@users.noreply.github.com>
2023-06-08 22:22:36 +0530

    Add files via upload

---
+++ CheckIntegerNumber.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CheckIntegerNumber{
+    public static void main(String[] args){
+        System.out.println("enter a number");
+        Scanner obj=new Scanner(System.in);
+        System.out.println(obj.hasNextInt());
+
+    }
+}
